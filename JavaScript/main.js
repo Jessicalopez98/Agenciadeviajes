bb284e71f0cf2789807c567c5bc8c57771124efe
Kenia <nkenia31459@gmail.com>
2025-08-01 06:52:07 -0600

    Se intentaron dos formas de animar el texto emergente en el carrusel sin exito en una sola noche kj, se dejan comentarios

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,1 +1,37 @@
 //Archivo Main para JaaScript Agencia de Viajes
+////// ESTA NO SE PUEDE, CREO PORQUE TENEMOS CARD IMAGE-HOVER
+// function mostrarDescripcion(id) {
+// let recuadroDescripcion = document.getElementById(id); //recuadro es igual a id div
+// if (recuadroDescripcion.style.display === "none") { //si se cumple mi condicion de que mi var recuadro.style.display ==="none"
+
+//     recuadroDescripcion.style.display = "block";      // aplica que recuadro.style.display = "block"
+//   } else {
+//     recuadroDescripcion.style.display = "none";       // si no,  aplica que recuadro.style.display = "none"
+//   }
+// }
+
+// OTRA FORMA DE APARECER EL TEXTO ERA CON UN BOTON
+
+// AGREGAR BOTON DESPUES DE LA DESCRIPCION DEL TEXTO sea que se convierta a parrafo o se quede como div
+// O agregar el boton afuera del carrusel pero creo que sin la propiedad flex para pasar de slide
+// <button onclick="ocultarDescripcion()">Cerrar</button>
+
+// CSS
+//  .overlay {
+//     display: flex;
+//     position: absolute;
+//     background-color:aqua;
+//     border: 1px solid #ccc;
+//     padding: 10px;
+//     z-index: 1;
+//   }
+
+//  function mostrarDescripcion() {
+//      document.getElementsByClassName("overlay").style.display = "block";
+//  }
+
+//  function ocultarDescripcion() {
+//      document.getElementsByClassName("overlay").style.display = "none";
+//  }
+
+//  document.getElementsByClassName("row").addEventListener("click", mostrarDescripcion);
