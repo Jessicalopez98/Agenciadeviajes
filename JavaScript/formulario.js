3dbdc44cdccb7abb644381799895a7525ba8b488
Leidi Elvira Lopez Silvan <silvanleidi@gmail.com>
2025-08-14 15:21:14 -0600

    Se crear formulario y modal de crear paquete

---
+++ JavaScript/formulario.js
@@ -0,0 +1,5 @@
+document.getElementById("boton-crear-paquete").addEventListener("click", () => {
+  // https://getbootstrap.com/docs/5.0/components/modal/
+  let modal = new bootstrap.Modal(document.querySelector(".modal"));
+  modal.show();
+});
