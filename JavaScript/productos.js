7e86db00f6d9c9d17073c13d73796bb58bebd0d0
Nadia Bustos Vazquez <nadiamickey.81@gmail.com>
2025-08-27 19:41:14 -0600

    Se corrigio responsividad en productos html y js adicional se agregaron styles NAD

--- JavaScript/productos.js
+++ JavaScript/productos.js
@@ -1,81 +1,69 @@
 function addItem(item) {
-  const itemHTML = '<div class="row">\n' +
-    '<div class="col-lg-3 col-sm">\n' +
-    '<article class="card cold fondo-color-card"\n' +
-    'style="width: 24rem; background-color: #f2f2f2; border-radius:15px ;">\n' +
-    '<header>\n' +
-    '<h5 class="card-title">' + item.titulo + '</h5>\n' +
-
-    '<img\n' +
-    'src="' + item.img + '"\n' +
-    'class="card-img-top" alt="Imagen de destino">\n' +
-    '</header>\n' +
-    '<footer-card>\n' +
-    '<h5 class="card-title" style="font-size:1.4rem"><p>Desde $'+ item.precio + ' USD por persona en habitación doble + IMP </p></h5> <!--aqui-->\n' +
-
-      '<div class="row">\n' +
-            '<div class="col-lg-6 col-sm">\n' +
-              '<ul>\n' +
-                '<li class="list-group-item"><i class="bi bi-airplane"></i> Vuelos</li>\n' +
-                '<li class="list-group-item"><i class="bi bi-buildings"></i> Hospedaje</li>\n' +
-                '<li class="list-group-item"><i class="bi bi-cup-hot"></i> Alimentos</li>\n' +
-                '<li class="list-group-item"><i class="bi bi-chat-dots"></i> Guía de habla hispana</li>\n' +
-                '<li class="list-group-item"><i class="bi bi-car-front"></i> Traslado</li>\n' +
-                '<li class="list-group-item"><i class="bi bi-geo-alt"></i> Visitas Guiadas</li>\n' +
-              '</ul>\n' +
-            '</div>\n' +
-
-
-            '<div class="col-lg-6 col-sm">\n' +
-              '<ul class="list-unstyled mb-2 mb-md-0">\n' +
-                '<i class="bi bi-sun-fill" id="dia"></i>'+ item.dias +' Días\n' +
-                '<i class="bi bi-moon" id="noches"></i>'+ item.noches +' Noches\n' +
-              '</ul>\n' +
-            '</div>\n' +
-
-      '</div>\n' +
-
-      '<div class="row">\n' +
+  const itemHTML = 
+  '<div class="col-lg-4 col-md-6 col-12 mb-4 col-sm">\n' +
+              '<article class="card fondo-color-card h-100 shadow-sm"\n' +
+              'style="background-color: #f2f2f2; border-radius:15px ;">\n' +
+              '<div class="card-body">\n' +
+              '<h5 class="card-title">' + item.titulo + '</h5>\n' +       
+              '<img\n' +
+              'src="' + item.img + '"\n' +
+              'class="card-img-top fixed-img" alt="Imagen de destino">\n' +
+              '<h5 class="card-title" style="font-size:1.4rem"><p>Desde $' + item.precio + ' USD por persona en habitación doble + IMP </p></h5> <!--aqui-->\n' +
+  
+    '<div class="row">\n' +
         '<div class="col-lg-6 col-sm">\n' +
-            '<!-- boton -->\n' +
-              '<div class="card-body" style="display: flex; justify-content: flex-start; align-items: end;">\n' +
-                `<button  style="border-radius: 31px; background-color: #A9D943; color: #FFFFFF; padding: .8rem 3.5rem; font-family: Montserrat; font-weight: bold; font-size: 1.2rem;  " type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#modal${item.id}">\n` +
-                'Reservar\n' +
-                '</button>\n' +
-              '</div>\n' +
-
+          '<ul>\n' +
+            '<li class="list-group-item"><i class="bi bi-airplane"></i> Vuelos</li>\n' +
+            '<li class="list-group-item"><i class="bi bi-buildings"></i> Hospedaje</li>\n' +
+            '<li class="list-group-item"><i class="bi bi-cup-hot"></i> Alimentos</li>\n' +
+            '<li class="list-group-item"><i class="bi bi-chat-dots"></i> Guía de habla hispana</li>\n' +
+            '<li class="list-group-item"><i class="bi bi-car-front"></i> Traslado</li>\n' +
+            '<li class="list-group-item"><i class="bi bi-geo-alt"></i> Visitas Guiadas</li>\n' +
+          '</ul>\n' +
+        '</div>\n' +
+        '<div class="col-lg-6 col-sm">\n' +
+            '<ul class="list-unstyled mb-2 mb-md-0">\n' +
+            '<i class="bi bi-sun-fill" id="dia"></i>' + item.dias + ' Días\n' +
+            '<i class="bi bi-moon" id="noches"></i>' + item.noches + ' Noches\n' +
+            '</ul>\n' +
         '</div>\n' +
       '</div>\n' +
 
-    '</footer-card>\n' +
+   
+            '<!-- boton -->\n' +
+            '<div class="row">\n' +
+            '<div class="card-body" style="display: flex; justify-content: flex-start; align-items: end;">\n' +
+            `<button  style="border-radius: 31px; background-color: #A9D943; color: #FFFFFF; padding: .8rem 3.5rem; font-family: Montserrat; font-weight: bold; font-size: 1.2rem;  " type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#modal${item.id}">\n` +
+            'Reservar\n' +
+            '</button>\n' +
+            '</div>\n' +
+            '</div>\n' +
     '</article>\n' +
-    '</div>\n' +
-    '</div>\n' +
+  '</div>\n' +
 
     '<!-- modal -->\n' +
-    `<div class="modal" id="modal${item.id}" tabindex="-1">\n` +
-    '<div class="modal-dialog">\n' +
-    '<div class="modal-content">\n' +
-        '<div class="modal-header">\n' +
-          '<h5 class="modal-title">' + item.titulo + '</h5>\n' +
-          '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>\n' +
+        `<div class="modal" id="modal${item.id}" tabindex="-1">\n` +
+            '<div class="modal-dialog">\n' +
+              '<div class="modal-content">\n' +
+                  '<div class="modal-header">\n' +
+                    '<h5 class="modal-title">' + item.titulo + '</h5>\n' +
+                    '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>\n' +
+                  '</div>\n' +
+                  '<div class="modal-body">\n' +
+                    '<p>' + item.incluye + item.noincluye + '</p>\n' +
+                    '<p><strong>Duración:</strong> ' + item.duracion + '</p>\n' +
+                    '<p><strong>Lugares:</strong> ' + item.lugar + '</p>\n' +
+                    '<p>Para más información, por favor contactanos.</p>\n' +
+                    '<p>¡Gracias por tu interés en nuestros paquetes turísticos!</p>\n' +
+                  '</div>\n' +
+                  '<div class="modal-footer">\n' +
+                    '<a href="./contactanos.html">\n' +
+                    '<button type="button" class="btn btn-primary">Contáctanos</button>\n' +
+                    '</a>\n' +
+                  '</div>\n' +
+              '</div>\n' +
+            '</div>\n' +
         '</div>\n' +
-        '<div class="modal-body">\n' +
-          '<p>' + item.incluye + item.noincluye + '</p>\n' +
-          '<p><strong>Duración:</strong> ' + item.duracion + '</p>\n' +
-          '<p><strong>Lugares:</strong> ' + item.lugar + '</p>\n' +
-          '<p>Para más información, por favor contactanos.</p>\n' +
-          '<p>¡Gracias por tu interés en nuestros paquetes turísticos!</p>\n' +
-        '</div>\n' +
-        '<div class="modal-footer">\n' +
-          '<a href="./contactanos.html">\n' +
-          '<button type="button" class="btn btn-primary">Contáctanos</button>\n' +
-          '</a>\n' +
-        '</div>\n' +
-    '</div>\n' +
-    '</div>\n' +
-    '</div>\n' +
-    //'</div>\n' +
     '<br/>';
 
   const itemsContainer = document.getElementById("list-items");
